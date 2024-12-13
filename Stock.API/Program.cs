f8522d346813a0d60c8684eb228fa6bed84cd3e0
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-12-13 21:54:16 +0300

    Stock.API' üzerinden MongoDb Veri tabanı konfigürasyon işlemleri yapıldıktan sonra , dummy data olarak veri eklendi.

--- Stock.API/Program.cs
+++ Stock.API/Program.cs
@@ -1,6 +1,10 @@
 using MassTransit;
+using MongoDB.Driver;
+using Stock.API.Models;
+using Stock.API.Services;
 //Consumer üzerinden iþlem yapýlacaðý için swaggerlarý falan Stock.API'dan sildik
 var builder = WebApplication.CreateBuilder(args);
+
 
 // MassTransit Konfigürasyonu ayarlarý
 builder.Services.AddMassTransit(configurator =>
@@ -11,7 +15,30 @@ builder.Services.AddMassTransit(configurator =>
     });
 });
 
+builder.Services.AddSingleton<MongoDbService>();
+
 var app = builder.Build();
 
+using var scope = builder.Services.BuildServiceProvider().CreateScope();
+var mongoDbService = scope.ServiceProvider.GetRequiredService<MongoDbService>();
+
+if(!await (await mongoDbService.GetCollection<Stock.API.Models.Stock>().FindAsync(x=> true)).AnyAsync())
+{
+    mongoDbService.GetCollection<Stock.API.Models.Stock>().InsertOne(new Stock.API.Models.Stock()
+    {
+        ProductId = 1,
+        Count = 200
+    });
+    mongoDbService.GetCollection<Stock.API.Models.Stock>().InsertOne(new Stock.API.Models.Stock()
+    {
+        ProductId = 2,
+        Count = 300
+    });
+    mongoDbService.GetCollection<Stock.API.Models.Stock>().InsertOne(new Stock.API.Models.Stock()
+    {
+        ProductId = 3,
+        Count = 69
+    });
+}
 
 app.Run();
