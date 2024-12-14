32c1fa954d9dda419d22b6b2a0e2318826ef02e0
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-12-14 12:39:43 +0300

    State Machine Projesinde program.cs'de gerekli konfigürasyon ayarları yapıldıktan sonra migration ve veri tabanı oluşturuldu.

--- SagaStateMachine.Service/Program.cs
+++ SagaStateMachine.Service/Program.cs
@@ -1,9 +1,21 @@
 using MassTransit;
+using Microsoft.EntityFrameworkCore;
+using SagaStateMachine.Service.StateDbContexts;
+using SagaStateMachine.Service.StateInstances;
+using SagaStateMachine.Service.StateMachines;
 
 var builder = Host.CreateApplicationBuilder(args);
 //Workerdada Payment.API ve Stock.API'da olduðu gibi 
 builder.Services.AddMassTransit(configurator =>
 {
+    //Masstransitte bu yapýlandýrmayý yapmamýz için AddSagaStateMachine generic olarak bir statemachine ardýndan da T parametresi olarak bir stateInstance istiyor.
+    configurator.AddSagaStateMachine<OrderStateMachine, OrderStateInstance>().EntityFrameworkRepository(options =>
+    {
+        options.AddDbContext<DbContext, OrderStateDbContext>((provider, _builder) =>
+        {
+            _builder.UseSqlServer(builder.Configuration.GetConnectionString("Default"));
+        });
+    });
     configurator.UsingRabbitMq((context, _configure) =>
     {
         _configure.Host(builder.Configuration["RabbitMQ"]);
