037229aa42bfb060f48fcec80c118b9e3ebc4dc8
EmreCanTERKAN <emrecanterkan06@gmail.com>
2024-12-16 20:29:33 +0300

    OrderCompletedConsumer ve OrderFailedConsumer tanımlandı

---
+++ Order.API/Consumers/OrderFailedEventConsumer.cs
@@ -0,0 +1,19 @@
+﻿using MassTransit;
+using Order.API.Context;
+using Shared.OrderEvents;
+
+namespace Order.API.Consumers
+{
+    public class OrderFailedEventConsumer(OrderDbContext orderDbContext) : IConsumer<OrderFailedEvent>
+    {
+        public async Task Consume(ConsumeContext<OrderFailedEvent> context)
+        {
+            Models.Order order = await orderDbContext.Orders.FindAsync(context.Message.OrderId);
+            if (order is not null)
+            {
+                order.OrderStatu = Enums.OrderStatus.Completed;
+                await orderDbContext.SaveChangesAsync();
+            }
+        }
+    }
+}
